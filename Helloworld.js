3614ee30a907d1f538bb74109f3906711ef693ea
terri <terripauly@gmail.com>
2020-05-29 16:36:48 -0700

    initialcommit

---
+++ Helloworld.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html> lang = "en-US">
+
+    <head>
+        <title> prompt.html</title>
+        /not sure what I"m doing/
+        <script type = "text/javascript">
+        var person = " "
+         person = prompt ("what is your name?")
+         alert ("hi");
+         alert (person);
+         </script>
+
+
+
+
+    </head>
+
+
+
+
+
+
+</html>
